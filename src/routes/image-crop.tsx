010d2e8bffa2deb3bf7fb1aec6e61166b9b78e2b
hutyxx <hutyxxx@gmail.com>
2026-02-13 15:24:30 +0800

    feat: external

--- src/routes/image-crop.tsx
+++ src/routes/image-crop.tsx
@@ -1,5 +1,7 @@
+import ImageCropTool from '@/components/ImageCropTool'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/image-crop')({
   ssr: false,
+  component: ImageCropTool
 })
--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,14 @@ import viteTsConfigPaths from 'vite-tsconfig-paths'
 import { cloudflare } from '@cloudflare/vite-plugin'
 import tailwindcss from '@tailwindcss/vite'
 
+const jsquashExternals = {
+  '@jsquash/avif': 'https://esm.sh/@jsquash/avif@2.1.1',
+  '@jsquash/jpeg': 'https://esm.sh/@jsquash/jpeg@1.6.0',
+  '@jsquash/png': 'https://esm.sh/@jsquash/png@3.1.1',
+  '@jsquash/webp': 'https://esm.sh/@jsquash/webp@1.5.0',
+  '@jsquash/resize': 'https://esm.sh/@jsquash/resize@2.1.1',
+}
+
 const config = defineConfig({
   resolve: {
     alias: {
@@ -28,15 +36,23 @@ const config = defineConfig({
   worker: {
     format: 'es',
     plugins: () => [viteTsConfigPaths()],
+    rollupOptions: {
+      external: Object.keys(jsquashExternals),
+      output: {
+        paths: jsquashExternals,
+      },
+    },
   },
   optimizeDeps: {
-    exclude: [
-      '@jsquash/avif',
-      '@jsquash/jpeg',
-      '@jsquash/png',
-      '@jsquash/webp',
-      '@jsquash/resize',
-    ],
+    exclude: Object.keys(jsquashExternals),
+  },
+  build: {
+    rollupOptions: {
+      external: Object.keys(jsquashExternals),
+      output: {
+        paths: jsquashExternals,
+      },
+    },
   },
 })
 
